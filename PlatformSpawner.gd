724d4e72ccf64704c25d565b761509e73893e9e0
ThisPerato <dariealessandro@gmail.com>
2025-04-26 19:40:24 +0200

    Impostata risoluzione mobile 480x800 e preparazione Camera2D

--- PlatformSpawner.gd
+++ PlatformSpawner.gd
@@ -8,7 +8,9 @@ func on_platform_reached(current_platform_y):
 	var new_platform = platform_scene.instantiate()
 	add_child(new_platform)
 
-	var x_position = randf_range(-150, 150)  # posizione random su X
-	var y_position = current_platform_y - platform_gap  # più in alto
+	var screen_width = 480
+	var half_width = screen_width / 2 - 60
+	var x_position = randf_range(-half_width, half_width)
+	var y_position = current_platform_y - platform_gap
 
 	new_platform.global_position = Vector2(x_position, y_position)
