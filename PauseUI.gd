56322ed30273c5ea6a2395e6dd6d137d91086c99
ThisPerato <dariealessandro@gmail.com>
2025-04-29 15:54:06 +0200

    Fixed generals layout

--- PauseUI.gd
+++ PauseUI.gd
@@ -9,10 +9,18 @@ func _ready():
 func show_pause():
 	visible = true
 	get_tree().paused = true
+	
+	var main = get_tree().get_root().get_node("Main")
+	if main.has_node("ScoreUI/Button_Pause"):
+		main.get_node("ScoreUI/Button_Pause").visible = false
 
 func hide_pause():
 	visible = false
 	get_tree().paused = false
+	
+	var main = get_tree().get_root().get_node("Main")
+	if main.has_node("ScoreUI/Button_Pause"):
+		main.get_node("ScoreUI/Button_Pause").visible = true
 
 func _on_button_continue_pressed():
 	await get_tree().create_timer(0.3).timeout
@@ -23,14 +31,6 @@ func _on_button_restart_pressed():
 	await get_tree().create_timer(0.3).timeout
 	hide_pause()
 	get_tree().reload_current_scene()
-
-
-func _on_button_options_pressed():
-	await get_tree().create_timer(0.3).timeout
-	hide_pause()
-	get_tree().paused = false
-	get_tree().change_scene_to_file("res://OptionsMenu.tscn")
-
 
 func _on_button_menu_pressed():
 	await get_tree().create_timer(0.3).timeout
